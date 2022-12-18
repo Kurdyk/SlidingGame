316cdeccc7c17de9fe52705e24ffa8b53c3346e5
Kurdyk <louis.kurdyk@etu.u-paris.fr>
2022-12-18 15:26:59 +0100

    Board shuffling based on instructions

--- src/main/java/Game/Board/Board.java
+++ src/main/java/Game/Board/Board.java
@@ -21,6 +21,13 @@ public class Board {
 
     }
 
+    public Board(TaquinBoardState boardState, CellFactory cellFactory, int shuffleDepth) {
+        this.boardState = boardState;
+        this.cellFactory = cellFactory;
+        this.shuffleInstruction(shuffleDepth);
+
+    }
+
     public TaquinBoardState getBoardState() {
         return boardState;
     }
@@ -33,6 +40,9 @@ public class Board {
         return boardState.getSize();
     }
 
+    /**
+     * Fully randomize the board, can lead to unsolvable configuration
+     */
     private void shuffle() {
         int nbOfCells = getSize() * getSize() - 1;
 
@@ -56,6 +66,55 @@ public class Board {
         }
 
         this.boardState.addCell(cellFactory.createEmpty(getSize() - 1, getSize() - 1)); // add empty cell
+    }
+
+    /**
+     * Shuffle the board with a certain number of randomly chosen moves without direct contradiction between moves (like
+     * up and directly after down).
+     * @param numberOfMoves the number of actions to perform
+     */
+    private void shuffleInstruction(int numberOfMoves) {
+
+        // Creating the cells
+        int count = 0;
+        for (int i = 0; i < getSize(); i++) {
+            for (int j = 0; j < getSize(); j++) {
+                if (i == getSize() - 1 && j == getSize() - 1) {
+                    // Skip the last position
+                    break;
+                }
+                this.boardState.addCell(cellFactory.createTaquinCell(count++, j, i)); // add cell with number
+            }
+        }
+
+        this.boardState.addCell(cellFactory.createEmpty(getSize() - 1, getSize() - 1)); // add empty cell
+
+        // Shuffling
+        Random random = new Random();
+        int lastDirectionChoice = -1; // direction of the move of the empty tile
+        for (int i = 0; i < numberOfMoves; i++) {
+            System.out.println("Shuffling step : " + i);
+            int choice = random.nextInt(4);
+            if (lastDirectionChoice != -1  && choice == ~lastDirectionChoice) { // direction
+                choice++;
+            }
+            TaquinCell empty = boardState.getEmptyPosition();
+            TaquinBoardDirection direction = switch (choice) {
+                case 0 -> TaquinBoardDirection.UP;
+                case 1 -> TaquinBoardDirection.LEFT;
+                case 2 -> TaquinBoardDirection.RIGHT;
+                default -> TaquinBoardDirection.DOWN;
+            };
+
+            if (!boardState.hasNeighbor(direction, empty)) { // illegal move
+                i--;
+                continue;
+            }
+
+            boardState.processInstruction(TaquinBoardInstruction.mapFromDirection(direction), empty);
+            lastDirectionChoice = choice;
+        }
+
     }
 
     @Override
