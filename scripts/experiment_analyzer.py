61dc2914493fa74c359d9b19d976d6fcc9823655
Ellington <ellingtonkirby31@gmail.com>
2022-12-26 11:25:33 -0500

    experiments: parse results and graph

--- scripts/experiment_analyzer.py
+++ scripts/experiment_analyzer.py
@@ -1,18 +1,37 @@
 import argparse
 import os
+import matplotlib.pyplot as plt
 
 def read_files_in_directory(directory):
   # List all the files in the directory
-  filenames = os.listdir(directory)
+    filenames = os.listdir(directory)
+    parsed_experiments = list()
 
   # Iterate over the files
-  for filename in filenames:
+    for filename in filenames:
     # Open the file and read its contents
-    with open(os.path.join(directory, filename)) as f:
-      contents = f.read()
+        with open(os.path.join(directory, filename)) as f:
+            file_dict = {}
+            for line in f:
+                splitted = line.split(':')
+                file_dict[splitted[0].rstrip()] = splitted[1].rstrip()
+            parsed_experiments.append(file_dict)
 
     # Print the contents of the file
-    print(contents)
+    return parsed_experiments
+
+def graph_experiment(parsed_experiments):
+    fig, ax = plt.subplots()
+    x = list()
+    y = list()
+    for experiment in parsed_experiments:
+        x.append(experiment['Heuristic'])
+        y.append(experiment['Max Frontier Size'])
+    
+    ax.bar(x, y)
+    ax.set_xlabel('Heuristic')
+    ax.set_ylabel('Max Frontier Size')
+    plt.show()
 
 if __name__ == "__main__":
   # Parse the command-line arguments
@@ -21,4 +40,4 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   # Read all the files in the specified directory
-  read_files_in_directory(args.directory)
+  graph_experiment(read_files_in_directory(args.directory))
